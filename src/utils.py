6f9abe245d57e8d6e9c6b8587b03be22f9ee268c
hattan al <hattan.ca@gmail.com>
2023-08-28 22:18:31 -0400

    Update

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,14 @@
-# import os 
-# import pickle
+import os 
+import pickle
 
 
-# def save_object(file_path, obj):
-#     try:
-#         dir_path = os.path.dirname(file_path)
-#         os.makedirs(dir_path, exist_ok=True)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
 
-#         with open(file_path, 'wb') as file_obj:
-#             pickle.dump(obj, file_obj)
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj, file_obj)
 
-#     except:
-#         pass
+    except:
+        pass
